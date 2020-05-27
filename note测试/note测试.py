b6b86944bd6895cf38928aa57bcabfbb26c15007
UncleDong <1758322248@qq.com>
2020-05-27 09:05:54 +0800

    --story=1003420 --user=董安宁 更改了音符生成规则

--- note测试/note测试.py
+++ note测试/note测试.py
@@ -1,8 +1,10 @@
 import pytest
 import json
-
-json_file = open("test_file.json",'r',encoding='gbk')  
+from utils import *
+json_file = open("test_file.json",'r',encoding='gbk')
+json_file2 = open("test_file2.json",'r',encoding='gbk')  
 a = json.load(json_file)
+b = json.load(json_file2)
 # 读取数据
 testname_list = []
 for i in range(len(a)):
@@ -11,5 +13,14 @@ for i in range(len(a)):
 
 # 进行测试
 @pytest.mark.parametrize('test_name', testname_list)
-def test_str2note(test_name):
+def test_str2note(test_name):    
     print(a[test_name])
+
+testname_list2 = []
+for i in range(len(b)):
+    testname_list2.append('test{0}'.format(i))
+# 进行测试2
+@pytest.mark.parametrize('test_name', testname_list)
+def test_str2note2(test_name):
+    assert b[test_name]['note_ans'] == str2tone(b[test_name]['note_str'])
+    print(b[test_name])
